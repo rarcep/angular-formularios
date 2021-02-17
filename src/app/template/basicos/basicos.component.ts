df9a48617c245f3b6de5c115fc0c80d73b1a01dd
rarcep <rarcep@gmail.com>
2021-02-16 23:36:34 -0600

    Seccion 16: Formularios

---
+++ src/app/template/basicos/basicos.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styleUrls: ['./basicos.component.css']
+})
+export class BasicosComponent implements OnInit {
+
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  nombreValido(): boolean {
+    return this.miFormulario?.controls.producto?.invalid
+            && this.miFormulario?.controls.producto.touched;
+  }
+
+  /* guardar(miFormulario: NgForm) { */
+  guardar() {
+    console.log(this.miFormulario);
+  }
+
+}
